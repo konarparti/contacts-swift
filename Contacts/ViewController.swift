b91ff456b1d8a9d7e8c090dc2a557485de203428
Danila Kisikov <kisikov03@mail.ru>
2024-12-07 13:52:45 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by R355-M10-Stud on 07.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
